0642eb9a3bd7989c2c47848bd3f0ad1f4a9a6967
Root <Root@Sudito>
2021-11-08 12:16:57 +0100

    Update

--- Bingo/src/Jugar.java
+++ Bingo/src/Jugar.java
@@ -1,19 +1,4 @@
-/**
- * 
- */
 
-/**
- * @author Root
- *
- */
 public class Jugar {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
